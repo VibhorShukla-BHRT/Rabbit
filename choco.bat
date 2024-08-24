8d7743d33f326926813cb13739a4115b0ca2ac01
VIBHOR SHUKLA <shukla.vibhor495@gmail.com>
2024-08-24 21:04:16 +0530

    First draft

---
+++ choco.bat
@@ -0,0 +1,36 @@
+@echo off
+:: Elevate the script to run as Administrator
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' NEQ '0' (
+    echo You need to run this script as Administrator.
+    pause
+    exit /b
+)
+
+:: Install OpenSSH Server
+echo Installing OpenSSH Server...
+powershell -Command "Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0"
+
+:: Start the SSH service
+echo Starting SSH service...
+powershell -Command "Start-Service sshd"
+
+:: Set SSH service to start automatically on boot
+echo Setting SSH service to start automatically...
+powershell -Command "Set-Service -Name sshd -StartupType 'Automatic'"
+
+:: Display current username
+echo.
+echo Current User:
+whoami
+
+:: Display IP Address(es)
+echo.
+echo IP Addresses:
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr "IPv4 Address"') do echo %%a
+
+echo.
+echo OpenSSH Server installed and configured successfully!
+pause
+
